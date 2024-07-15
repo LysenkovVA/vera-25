feb92ac379bcc4689a0adab6be7c3f53b07a6cf0
LysenkovVA <lysenkov.v.a@yandex.ru>
2024-07-15 22:19:41 +0300

    Add auth.js to package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  throw new Error("Test error")
-  return <div>Добро пожаловать на главную страницу!</div>
+  return <div>Добро пожаловать на главную страницу!</div>;
 }
