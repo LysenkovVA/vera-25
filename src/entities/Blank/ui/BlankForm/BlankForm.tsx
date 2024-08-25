667c6664788e936415fba27e9880a0e0a8657330
LysenkovVA <lysenkov.v.a@yandex.ru>
2024-08-25 22:03:37 +0300

    add icons

--- src/entities/Blank/ui/BlankForm/BlankForm.tsx
+++ src/entities/Blank/ui/BlankForm/BlankForm.tsx
@@ -11,7 +11,21 @@ import {
   Tabs,
   Typography,
 } from "antd";
-import { MinusCircleFilled, PlusCircleFilled } from "@ant-design/icons";
+import {
+  ApiFilled,
+  AppstoreTwoTone,
+  BookFilled,
+  ContactsFilled,
+  ContainerFilled,
+  EnvironmentFilled,
+  FileImageTwoTone,
+  HomeFilled,
+  InfoCircleTwoTone,
+  LockFilled,
+  MinusCircleFilled,
+  PlusCircleFilled,
+  ToolFilled,
+} from "@ant-design/icons";
 import { SecurityLevelSelector } from "@/features/SecurityLevelSelector";
 import { CountrySelector } from "@/features/CountrySelector";
 import { ManufacturerSelector } from "@/features/ManufacturerSelector";
@@ -38,21 +52,50 @@ const BlankForm = (props: BlankDescriptionFormProps) => {
   const infoFormContent = (
     <>
       <Divider orientation={"left"}>
-        <Typography.Title level={4}>Общие сведения</Typography.Title>
+        <Typography.Title level={4}>
+          <Flex gap={4}>
+            <InfoCircleTwoTone />
+            {"Общие сведения"}
+          </Flex>
+        </Typography.Title>
       </Divider>
-      <Form.Item label={"Название"} name={"name"}>
+      <Form.Item label={<Flex gap={4}>{"Название"}</Flex>} name={"name"}>
         <Input.TextArea
           placeholder={"Укажите название бланка"}
           autoSize={{ minRows: 3, maxRows: 3 }}
         />
       </Form.Item>
-      <Form.Item label={"Страна"} name={"countryId"}>
+      <Form.Item
+        label={
+          <Flex gap={4}>
+            <HomeFilled />
+            {"Страна"}
+          </Flex>
+        }
+        name={"countryId"}
+      >
         <CountrySelector placeholder={"Укажите страну"} />
       </Form.Item>
-      <Form.Item label={"Производитель"} name={"manufacturerId"}>
+      <Form.Item
+        label={
+          <Flex gap={4}>
+            <ToolFilled />
+            {"Производитель"}
+          </Flex>
+        }
+        name={"manufacturerId"}
+      >
         <ManufacturerSelector placeholder={"Укажите производителя"} />
       </Form.Item>
-      <Form.Item label={"Уровень защищенности"} name={"securityLevelId"}>
+      <Form.Item
+        label={
+          <Flex gap={4}>
+            <LockFilled />
+            {"Уровень защищенности"}
+          </Flex>
+        }
+        name={"securityLevelId"}
+      >
         <SecurityLevelSelector placeholder={"Укажите уровень защищенности"} />
       </Form.Item>
     </>
@@ -362,23 +405,53 @@ const BlankForm = (props: BlankDescriptionFormProps) => {
   // Персонализация
   const personalizationFormContent = <>{"Контент персонализации"}</>;
 
-  const tabsItems: { label: string; key: string; children: React.ReactNode }[] =
-    [
-      { label: "Обложка", key: "1", children: coverFormContent },
-      { label: "Блок", key: "2", children: blockFormContent },
-      { label: "Скрепление", key: "3", children: fasteningFormContent },
-      { label: "Реквизиты", key: "4", children: detailsLocationFormContent },
-      {
-        label: "Персонализация",
-        key: "5",
-        children: personalizationFormContent,
-      },
-    ];
+  const tabsItems: {
+    label: string;
+    key: string;
+    children: React.ReactNode;
+    icon?: React.ReactNode;
+  }[] = [
+    {
+      label: "Обложка",
+      key: "1",
+      children: coverFormContent,
+      icon: <BookFilled />,
+    },
+    {
+      label: "Блок",
+      key: "2",
+      children: blockFormContent,
+      icon: <ContainerFilled />,
+    },
+    {
+      label: "Скрепление",
+      key: "3",
+      children: fasteningFormContent,
+      icon: <ApiFilled />,
+    },
+    {
+      label: "Реквизиты",
+      key: "4",
+      children: detailsLocationFormContent,
+      icon: <EnvironmentFilled />,
+    },
+    {
+      label: "Персонализация",
+      key: "5",
+      children: personalizationFormContent,
+      icon: <ContactsFilled />,
+    },
+  ];
 
   const constructionFormContent = (
     <>
       <Divider orientation={"left"}>
-        <Typography.Title level={4}>Конструкция</Typography.Title>
+        <Typography.Title level={4}>
+          <Flex gap={4}>
+            <AppstoreTwoTone />
+            {"Конструкция"}
+          </Flex>
+        </Typography.Title>
       </Divider>
       <Form.Item colon={false} label={" "}>
         <Tabs tabPosition={"top"} items={tabsItems} />
@@ -389,7 +462,12 @@ const BlankForm = (props: BlankDescriptionFormProps) => {
   const imagesFormContent = (
     <>
       <Divider orientation={"left"}>
-        <Typography.Title level={4}>Изображения</Typography.Title>
+        <Typography.Title level={4}>
+          <Flex gap={4}>
+            <FileImageTwoTone />
+            {"Изображения"}
+          </Flex>
+        </Typography.Title>
       </Divider>
     </>
   );
